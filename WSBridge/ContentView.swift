001a447c08840fae9ef8059a7cbbd4a59b7b5c85
Jay <wjdwjd1501@gmail.com>
2025-05-24 13:06:19 +0200

    Initial Commit

---
+++ WSBridge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WSBridge
+//
+//  Created by JungWonJung on 24/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WSBridge/WSBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WSBridgeApp.swift
+//  WSBridge
+//
+//  Created by JungWonJung on 24/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct WSBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
